fddebb1d290253b42779a9e8372a52095e132153
Ronaldo Noberto <103396752+Ronaldo913@users.noreply.github.com>
2022-08-23 08:08:33 -0300

    Add files via upload

---
+++ lib/domain/pacote_estudo.dart
@@ -0,0 +1,27 @@
+class PacoteEstudo {
+  final String imagem;
+  final String titulo;
+  final int numParcelas;
+  final int desconto;
+  final double precoAtual;
+  final double precoAntigo;
+  final int redacao;
+  final int aula;
+  final int exercicio;
+  final int horas;
+  final int duvida;
+
+  PacoteEstudo({
+    required this.desconto,
+    required this.imagem,
+    required this.titulo,
+    required this.numParcelas,
+    required this.precoAtual,
+    required this.precoAntigo,
+    required this.redacao,
+    required this.aula,
+    required this.exercicio,
+    required this.horas,
+    required this.duvida,
+  });
+}
