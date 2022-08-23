4ec29b448a64db6661a498068173c11b18a9d041
Ronaldo913 <rns6@aluno.ifal.edu.br>
2022-08-23 08:02:27 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:learnit/pages/home_page.dart';
+import 'package:learnit/pages/animation.dart';
+import 'package:learnit/pages/login_page.dart';
+
+void main() {
+  runApp(
+    //ChangeNotifierProvider(
+    //create: (context) => UsuarioRepository(),
+    //child:
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: AnimationPage(page: Loginpage()),
+      ));
+}
