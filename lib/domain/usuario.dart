7a3792c6a8e4e7e4947437fcb8a376e55b88a7de
Ronaldo Noberto <103396752+Ronaldo913@users.noreply.github.com>
2022-08-29 18:22:11 -0300

    Update usuario.dart

--- lib/domain/usuario.dart
+++ lib/domain/usuario.dart
@@ -1,1 +1,21 @@
-a
+class Usuario {
+  final String nome;
+  final String sobrenome;
+  final String nascimento;
+  final String celular;
+  final String email;
+  final String cpf;
+  final String username;
+  final String senha;
+
+  Usuario({
+    required this.nome,
+    required this.sobrenome,
+    required this.nascimento,
+    required this.celular,
+    required this.email,
+    required this.cpf,
+    required this.username,
+    required this.senha,
+  });
+}
