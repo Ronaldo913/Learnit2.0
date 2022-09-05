d8edfd1662b124d1a090f90a02303719971bc84d
SouNouxx <103265116+SouNouxx@users.noreply.github.com>
2022-09-05 11:04:56 -0300

    Update bd.dart

--- lib/data/bd.dart
+++ lib/data/bd.dart
@@ -77,13 +77,15 @@ class BD {
     await Future.delayed(const Duration(seconds: 3));
     return lista1;
   }
+  
   static List<Questoes> listaquest= [
     Questoes(titulo: 'Citologia'),
     Questoes(titulo: 'Embriologia'),
     Questoes(titulo: 'Génetica')
   ];
-  static Future<List<Questoes>> getPacotesTuristicos() async {
+  static Future<List<Questoes>> getListaQuest() async {
     await Future.delayed(const Duration(seconds: 3));
-    return lista;
+    return listaquest;
   }
+  
 }
