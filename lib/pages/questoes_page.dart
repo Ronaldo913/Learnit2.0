f0658bf404c6044f4d0f1d8703d39fc183106521
mayarinhan <103398087+mayarinhan@users.noreply.github.com>
2022-09-05 15:24:09 -0300

    Update questoes_page.dart

--- lib/pages/questoes_page.dart
+++ lib/pages/questoes_page.dart
@@ -18,27 +18,7 @@ class _QuestoesPageState extends State<QuestoesPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[200],
-      appBar: AppBar(
-        title: Text('MENU - QUESTÕES'),
-        backgroundColor: Color(0xFF0B4619),
-        toolbarHeight: 64,
-        actions: [
-          IconButton(
-            icon: Icon(Icons.home),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const HomePage();
-                  },
-                ),
-              );
-            },
-          ),
-          // add more IconButton
-        ],
-      ),
+
       body: Padding(padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
