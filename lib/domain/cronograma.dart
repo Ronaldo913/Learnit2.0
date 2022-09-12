dedbf3da76e20247e0b879986fb38e29b1f269c5
SouNouxx <103265116+SouNouxx@users.noreply.github.com>
2022-09-12 11:39:39 -0300

    Update cronograma.dart

--- lib/domain/cronograma.dart
+++ lib/domain/cronograma.dart
@@ -1,1 +1,15 @@
+class Cronograma {
+  final String title;
+  final int hour;
+  final int minute;
+  final String day;
+  final int color;
 
+  Cronograma({
+    required this.title,
+    required this.hour,
+    required this.minute,
+    required this.day,
+    required this.color,
+  });
+}
