bce7e625b7320f1398658307a67694d46af15534
JhenifferMaia <100545025+JhenifferMaia@users.noreply.github.com>
2022-10-11 08:36:12 -0300

    Create conteudo.dart

---
+++ lib/domain/conteudo.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Conteudo {
+  final String paragrafo1;
+  final String paragrafo2;
+  final String titulo2;
+  final String subtitulo;
+  final String paragrafo3;
+  final String subtitulo2;
+  final String paragrafo4;
+  final String titulo3;
+  final String paragrafo5;
+  final String subtitulo3;
+  final String paragrafo6;
+  final String subtitulo4;
+  final String paragrafo7;
+  final String paragrafo8;
+  final String subtitulo5;
+  final String paragrafo9;
+  final String espaco1;
+  final String imagem;
+  final String espaco2;
+  
+  Conteudo({
+    required this.paragrafo1,
+    required this.paragrafo2,
+    required this.titulo2,
+    required this.subtitulo,
+    required this.paragrafo3,
+    required this.subtitulo2,
+    required this.paragrafo4,
+    required this.titulo3,
+    required this.paragrafo5,
+    required this.subtitulo3,
+    required this.paragrafo6,
+    required this.subtitulo4,
+    required this.paragrafo7,
+    required this.paragrafo8,
+    required this.subtitulo5,
+    required this.paragrafo9,
+    required this.espaco1,
+    required this.imagem,
+    required this.espaco2,
+  });
+}
+
