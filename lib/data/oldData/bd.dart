54ad35c57d947ed8c8d6f69b5c9bd27ea4150ab9
felype15092004 <mfs22@aluno.ifal.edu.br>
2022-10-24 12:20:12 -0300

    teste

--- lib/data/oldData/bd.dart
+++ lib/data/oldData/bd.dart
@@ -12,55 +12,6 @@ import '../../domain/conteudo.dart';
 import '../../pages/contents/conteudo2_page.dart';
 
 class BD {
-  static final List<PacoteEstudo> lista1 = [
-    PacoteEstudo(
-      imagem: 'https://ronaldo913.github.io/ImagensPMovel/images/foco.png',
-      titulo: 'Pacote Foco',
-      desconto: 10,
-      numParcelas: 6,
-      precoAntigo: 19.90,
-      precoAtual: 9.90,
-      redacao: 1,
-      aula: 100,
-      exercicio: 300,
-      horas: 10,
-      duvida: 0,
-    ),
-    PacoteEstudo(
-      imagem:
-          'https://ronaldo913.github.io/ImagensPMovel/images/avan%C3%A7ado.png',
-      titulo: 'Pacote Médio',
-      desconto: 10,
-      numParcelas: 8,
-      precoAntigo: 35.00,
-      precoAtual: 20.00,
-      redacao: 3,
-      aula: 150,
-      exercicio: 500,
-      horas: 15,
-      duvida: 1,
-    ),
-    PacoteEstudo(
-      imagem: 'https://ronaldo913.github.io/ImagensPMovel/images/learnmed.png',
-      titulo: 'Pacote LearnMed',
-      desconto: 10,
-      numParcelas: 10,
-      precoAntigo: 50.00,
-      precoAtual: 30.00,
-      redacao: 5,
-      aula: 200,
-      exercicio: 700,
-      horas: 20,
-      duvida: 3,
-    ),
-  ];
-
-  static Future<List<PacoteEstudo>> getPacoteEstudo() async {
-    await Future.delayed(const Duration(seconds: 1));
-    return lista1;
-  }
-
-
   static List<Cronograma> listacrono = [
     Cronograma(
         title: 'Estudar Embriologia',
